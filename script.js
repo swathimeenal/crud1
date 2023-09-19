e37463beab5f59f3aa3b2871fce045759ed58957
swathimeenal <swathimeenal1994@gmail.com>
2023-09-19 20:33:25 +0530

    base

---
+++ script.js
@@ -0,0 +1,16 @@
+//!initial values
+
+
+//! get method(Retriving the data)
+
+
+//! insert the data(post method)
+
+
+//!edit and update the data(update method)
+
+
+//!deleting the data(delete method)
+
+
+//!reserting the values in form
